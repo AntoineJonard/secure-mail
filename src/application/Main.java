e403088c374afad829838bb6da9ec076b02305cf
AntoineJonard <antoine.jonard@orange.fr>
2022-03-01 17:42:33 +0100

    creation controller + view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,32 @@
 package application;
 	
+import java.io.IOException;
+
+import controller.ConnectionController;
+import controller.Controller;
+import controller.MailboxController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	private Stage primaryStage;
+	private VBox mailboxLayout;
+	private VBox connectionLayout;
+	private BorderPane rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Secure Mail");
+		
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
@@ -22,5 +40,56 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private void initRootLayout() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../view/RootLayout.fxml"));
+		
+		try {
+			rootLayout = (BorderPane) loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(rootLayout);
+		primaryStage.setScene(scene);		
+		primaryStage.show();	
+	}
+	
+	public void showConnectionView() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../view/Connexion.fxml"));
+		
+		Controller controller = new ConnectionController();
+		loader.setController(controller);
+		controller.setMain(this);
+		
+		try {
+			connectionLayout = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		rootLayout.setCenter(connectionLayout);
+	}
+	
+	public void showMailboxView() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../view/Mailbox.fxml"));
+		
+		Controller controller = new MailboxController();
+		loader.setController(controller);
+		controller.setMain(this);
+		
+		try {
+			mailboxLayout = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		rootLayout.setCenter(mailboxLayout);
 	}
 }
---
+++ src/controller/MailboxController.java
@@ -0,0 +1,5 @@
+package controller;
+
+public class MailboxController extends Controller{
+
+}
