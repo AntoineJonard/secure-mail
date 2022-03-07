25ce9e6e39c419f7d840ff58807c7a3eaf275705
paul <paul.canchon1@outlook.fr>
2022-03-07 17:03:27 +0100

    ajt SendMail.fxml

---
+++ src/controller/SendMailController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SendMailController extends Controller {
+
+	@FXML
+	private TextField Reciptient;
+	@FXML
+	private TextField Subject;
+	@FXML 
+	private TextField Message;
+	
+	@FXML
+	private void SendMail(MouseEvent event) {
+		
+	}
+	
+	@FXML
+	private void AddAttach(MouseEvent event) {
+		
+	}
+}
