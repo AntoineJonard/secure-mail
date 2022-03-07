11c441759533f54dc3b352d57db64deb05d8726f
AntoineJonard <antoine.jonard@orange.fr>
2022-03-07 03:03:36 +0100

    local

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
